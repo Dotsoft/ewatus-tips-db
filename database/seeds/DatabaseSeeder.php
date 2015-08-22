8a97bb902fac231de0daf41dc09d2dab6241dd6b
George Antoniadis <george@noodles.gr>
2015-08-22 17:52:03 +0300

    Seed locales

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\Locale;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,8 +17,32 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(LocaleTableSeeder::class);
 
         Model::reguard();
     }
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(['name' => 'John Doe', 'email' => 'user@foobar.com', 'password' => bcrypt('password')]);
+    }
+
+}
+
+class LocaleTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('locales')->delete();
+
+        Locale::create(['subtag' => 'en', 'description' => 'English']);
+        Locale::create(['subtag' => 'el', 'description' => 'Greek']);
+    }
+
 }
